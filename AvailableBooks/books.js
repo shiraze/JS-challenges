c1f6d9cb48e618fbce4eaeea3fba356f1b501d68
Shiraz Esat <shiraz.esat@gmail.com>
2022-09-28 14:40:59 +0100

    Book class created

---
+++ AvailableBooks/books.js
@@ -0,0 +1,24 @@
+export class Book {
+  constructor(title, author, ISBN, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies;
+  }
+
+  getAvailability() {
+    return !this.numCopies
+      ? "out of stock"
+      : this.numCopies < 10
+      ? "low stock"
+      : "in stock";
+  }
+
+  sell(numSold = 1) {
+    this.numCopies -= numSold;
+  }
+
+  restock(numCopies = 5) {
+    this.numCopies += numCopies;
+  }
+}
---
+++ AvailableBooks/books.test.js
@@ -0,0 +1,89 @@
+import { Book } from "./books";
+
+describe("Books", () => {
+  it("should support creation of a book", () => {
+    const firstBook = new Book("Fairy Tale", "Stephen King", "11111", 5);
+
+    expect(firstBook).toBeInstanceOf(Book);
+  });
+
+  describe("getAvailability", () => {
+    it("should correctly indicate no stock", () => {
+      const firstBook = new Book("Fairy Tale", "Stephen King", "11111", 0);
+
+      expect(firstBook.getAvailability()).toBe("out of stock");
+    });
+
+    it("should correctly indicate low stock", () => {
+      const firstBook = new Book("Fairy Tale", "Stephen King", "11111", 8);
+
+      expect(firstBook.getAvailability()).toBe("low stock");
+    });
+
+    it("should correctly indicate in stock", () => {
+      const firstBook = new Book("Fairy Tale", "Stephen King", "11111", 13);
+
+      expect(firstBook.getAvailability()).toBe("in stock");
+    });
+  });
+
+  describe("sell", () => {
+    let firstBook;
+    beforeEach(() => {
+      firstBook = new Book("Fairy Tale", "Stephen King", "11111", 11);
+    });
+
+    it("should correctly reduce stock levels by one if amount not provided", () => {
+      expect(firstBook.getAvailability()).toBe("in stock");
+
+      firstBook.sell();
+      firstBook.sell();
+
+      expect(firstBook.getAvailability()).toBe("low stock");
+
+      while (firstBook.numCopies) {
+        firstBook.sell();
+      }
+      expect(firstBook.getAvailability()).toBe("out of stock");
+    });
+
+    it("should correctly reduce stock levels by amount specified", () => {
+      expect(firstBook.getAvailability()).toBe("in stock");
+
+      firstBook.sell(2);
+      expect(firstBook.getAvailability()).toBe("low stock");
+
+      firstBook.sell(9);
+      expect(firstBook.getAvailability()).toBe("out of stock");
+    });
+  });
+
+  describe("restock", () => {
+    let firstBook;
+    beforeEach(() => {
+      firstBook = new Book("Fairy Tale", "Stephen King", "11111", 0);
+    });
+
+    it("should correctly increase stock levels by five if amount not provided", () => {
+      expect(firstBook.getAvailability()).toBe("out of stock");
+
+      firstBook.restock();
+
+      expect(firstBook.getAvailability()).toBe("low stock");
+
+      firstBook.restock();
+      expect(firstBook.getAvailability()).toBe("in stock");
+    });
+
+    it("should correctly increase stock levels by amount specified", () => {
+      expect(firstBook.getAvailability()).toBe("out of stock");
+
+      firstBook.restock(9);
+
+      expect(firstBook.getAvailability()).toBe("low stock");
+
+      firstBook.restock(2);
+      expect(firstBook.getAvailability()).toBe("in stock");
+    });
+  });
+});
